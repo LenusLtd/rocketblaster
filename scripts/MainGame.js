15d11ba442dc80758fc73dde2eb4aba07fbf1ae0
LenusLtd <Jordon@acusu.com>
2017-05-18 11:09:07 +0100

    Updated Scripting
    Adding script to develop the game

--- scripts/MainGame.js
+++ scripts/MainGame.js
@@ -1,14 +1,26 @@
 BasicGame.Game = function (game) {};
 
+// Grapghical Objects
+var ship;
+
 BasicGame.Game.prototype = {
 
 	create: function () {
-       
+       //Specify the pysics of the game to ARCADE
+			 this.physics.startsystem(Phaser.Physics.ARCADE);
+			 //Add the starfield and logo on screen
+			 this.starfield = this.add.tileSprite(0,0,800,600) 'starfield');
+			 //Add the ship on the screen, set physics and the boundaries
+			 ship = this.add.sprite((this.world.width /2,) this.world.height -50, 'ship');
+			 ship.anchor.setTo(0.5,0);
+			 this.physics.enable(ship, Phaser.Physics.ARCADE);
+			 ship.body.collideWorldBounds = True;
+
 	},
 
 	update: function () {
 		//execute 'createUfo','createLife','moveShip','collisionDetection' function
-		
+
 	}
 
 };
--- scripts/MainMenu.js
+++ scripts/MainMenu.js
@@ -1,5 +1,20 @@
 BasicGame.MainMenu = function (game) { };
 
+var startButton;
+var startfield;
+var logo;
+//We've already loaded the assets so well move straight into the MainMenu
+//All we are doing here is playing music, adding a picture and a button
+//I will modify the MainMenu To suit my game
+
+//output Sky,Ship,Score,Lives, Total, Start time to the screen
+//The scrolling Starfield Background
+starfield=  = this.add.titleSprite(0,0,800,600, 'starfield');
+logo= this.add.sprite((this.world.width /2), (this.world.height /2)-150,'logo');// FIX
+logo.anchor.setTo(0.5,0.5);
+startButton = this.add.button((this.world.width/2),(this.world.height/2) +50 'startButton', this.startGame);
+startButton.anchor.setTo(0.5,0.5);
+
 BasicGame.MainMenu.prototype = {
 
 	create: function () {
@@ -11,3 +26,11 @@ BasicGame.MainMenu.prototype = {
 	}
 
 };
+
+
+startGame: function(){
+//Add Start the actual game
+this.game.state.start ('Game');
+
+
+}
